c541cb68319a7239fe1e03c1587f20c3a8fea580
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-12-10 15:58:39 +0100

    Fixed tabs

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from "react";
 
-const App: FC = (): JSX.Element => {
-  return (
-    <div>
-      <h1>Boilerplate</h1>
-      <p>Some boilerplate code</p>
-    </div>
-  );
-};
+const App: FC = (): JSX.Element => (
+  <div>
+    <h1>Boilerplate</h1>
+    <p>Some boilerplate code</p>
+  </div>
+);
 
 export default App;
