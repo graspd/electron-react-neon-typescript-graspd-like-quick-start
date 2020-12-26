55bd8227ae85e1775e734619224582d5139335d8
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-12-26 23:16:53 +0100

    Fixed package versions

--- src/native-addon/native/src/lib.rs
+++ src/native-addon/native/src/lib.rs
@@ -4,6 +4,4 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
-register_module!(mut cx, {
-    cx.export_function("hello", hello)
-});
+register_module!(mut cx, { cx.export_function("hello", hello) });
